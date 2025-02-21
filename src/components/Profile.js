c9d63fa4e64dc020da81af4bffc917b6cc67a309
mahesh42646 <maheshdarkunde2@gmail.com>
2025-02-21 20:58:41 +0530

    mahesh

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome user</h1>
+      <p>This is the profile page.</p>
+    </div>
+  );
+}
+
+export default Profile;
